626220875b46a580ca2c7313cce8133b86b52639
Dmitriy <dmitriy.tereshchenko1989@mail.ru>
2021-06-05 19:13:25 +0300

    commit

---
+++ Controllers/ResourceController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestTaskBrokereeSolutions.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestTaskBrokereeSolutions.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourceController : ControllerBase
+    {
+        private ResourceDataContext dataContext = new ResourceDataContext();
+        
+        // GET: api/<ResourceController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Resource>>> GetAll()
+        {
+            var result = await Task.Run(() => dataContext.GetResources());
+            return new JsonResult(result);
+        }
+
+        // GET api/<ResourceController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Resource>> GetResourceDetail(int id)
+        {
+            var result = await Task.Run(() => dataContext.GetResourceFromId(id));
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return new JsonResult(result);
+        }
+
+        // POST api/<ResourceController>
+        [HttpPost]
+        public async Task<ActionResult<Resource>> Post(Resource resource)
+        {
+            await Task.Run(() => dataContext.SaveResource(resource));
+            return CreatedAtAction("GetResourceDetail", new { id = resource.Id }, resource);
+        }
+
+        // PUT api/<ResourceController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, Resource value)
+        {
+            var resource = await Task.Run(() => dataContext.GetResourceFromId(id));
+            if (resource is null)
+            {
+                return NotFound();
+            }
+
+            dataContext.UpdateResource(value);
+            return NoContent();
+
+        }
+
+        // DELETE api/<ResourceController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var result = await Task.Run(() => dataContext.GetResourceFromId(id));
+            if (result is null)
+            {
+                return BadRequest();
+            }
+
+            dataContext.DeleteResource(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Models/ResourceDataContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace TestTaskBrokereeSolutions.Models
+{
+    public class ResourceDataContext
+    {
+        private static ObservableCollection<Resource> resourceList;
+        static ResourceDataContext()
+        {
+            resourceList = new ObservableCollection<Resource>()
+            {
+                new Resource{Id=1, Name="foo"},
+                new Resource{Id=2, Name="bar"},
+                new Resource{Id=3, Name="baz"},
+                new Resource{Id=4, Name="test"},
+            };
+        }
+
+        public IEnumerable<Resource> GetResources() => resourceList;
+        public Resource GetResourceFromId(int id) => resourceList.FirstOrDefault(val => id == val.Id);
+        public void SaveResource(Resource resource) => resourceList.Add(resource);
+        public void DeleteResource(int id) => resourceList.Remove(resourceList.FirstOrDefault(val => id == val.Id));
+        public void UpdateResource(Resource resource)
+        {
+            var result = resourceList.FirstOrDefault(val => resource.Id == val.Id);
+            if (result != null)
+            {
+                result.Name = resource.Name;
+            }
+        }
+
+    }
+}
